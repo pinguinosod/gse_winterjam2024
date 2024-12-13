7b53d65160c4bedd8c3ecd7f5dc81c2d7e8dfeef
Lights <diegomarillanblooop@gmail.com>
2024-12-13 22:22:40 +0100

    Interactive and non-Interactive Objects
    Objects

--- weapons.gd
+++ weapons.gd
@@ -1,11 +1,36 @@
 extends Node3D
 
+# Maximum number of uses before the weapon is destroyed
+const MAX_USES: int = 10
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+# Counter for the number of uses
+var uses: int = 0
+
+# Called to perform an attack
+func attack() -> void:
+	if uses < MAX_USES:
+		perform_melee_attack()
+		uses += 1
+		print("Attack performed. Remaining uses: %d" % (MAX_USES - uses))
+		if uses >= MAX_USES:
+			queue_free()  # Removes the weapon node
+			print("The weapon has been destroyed.")
+	else:
+		print("The weapon can no longer be used.")
+
+# Melee attack logic
+func perform_melee_attack() -> void:
+	# Add collision, damage, or other logic here
+	print("Melee attack executed.")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#??????cLASS FOR Player???
+# Reference to the equipped weapon
+var equipped_weapon: Node3D = null
+
+# Called when the player attacks
+func perform_attack() -> void:
+	if equipped_weapon:
+		equipped_weapon.attack()
+	else:
+		print("No weapon equipped.")
