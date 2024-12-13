a872e39d5ab57fba5ed0983784da9323f5d9c97b
lhalbritter-tu <leo.halbritter@hotmail.com>
2024-12-13 22:52:18 +0100

    Load in Wave and Enemies works now

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -7,8 +7,9 @@ class_name GameManager
 @export var maxEnemyPoolSize = 100
 @export var enemyScene: PackedScene
 @export var enemyPoolHolder: Node3D
-var enemyPool: Array
+var enemyPool: Array[Node3D]
 var currentScene = 0
+var currentWave = 0
 var currentEnemyWave: EnemyWave
 var currentCombatScene: CombatScene
 
@@ -17,9 +18,11 @@ func _ready() -> void:
 	for i in range(maxEnemyPoolSize):
 		var instance = enemyScene.instantiate()
 		enemyPoolHolder.add_child(instance)
+		enemyPool.append(instance)
 		# instance.position = Vector3(i * (-1 ** i), 0, i * (-1 ** i))
 		instance.set_process(false)
 		instance.hide()
+	load_next_scene()
 	pass # Replace with function body.
 
 
@@ -28,9 +31,40 @@ func load_next_scene():
 		return
 	currentCombatScene = combatScenesToRun[currentScene]
 	currentScene += 1
+	currentWave = 0
+	load_next_wave()
 
 func load_next_wave():
-	pass
+	if currentWave >= len(currentCombatScene.enemyWaves):
+		return
+	currentEnemyWave = currentCombatScene.enemyWaves[currentWave]
+	currentWave += 1
+	load_in_enemies()
+	
+func load_in_enemies():
+	if is_wave_active():
+		return
+	# TODO Figure out enemy positions
+	for i in range(currentEnemyWave.basicEnemyCount):
+		var enemy = spawn_next_free_enemy()
+		# set hp to 1
+	for i in range(currentEnemyWave.eliteEnemyCount):
+		var enemy = spawn_next_free_enemy()
+		# set hp to 2
+
+func is_wave_active():
+	for enemy in enemyPool:
+		if enemy.visible:
+			return true
+	return false
+	
+func spawn_next_free_enemy():
+	for enemy in enemyPool:
+		if not enemy.visible:
+			enemy.show()
+			enemy.set_process(true)
+			return enemy
+	print("No free enemy found? Try increasing the pool size.")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
