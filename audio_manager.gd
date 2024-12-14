f89c9065b79114bbf1f20e4f30aee7bc1b634479
lhalbritter-tu <leo.halbritter@hotmail.com>
2024-12-14 16:09:36 +0100

    Audio Management

---
+++ audio_manager.gd
@@ -0,0 +1,20 @@
+extends AudioStreamPlayer3D
+
+var sfx_player
+
+func _ready() -> void:
+	self.play()
+	sfx_player = AudioStreamPlayer3D.new()
+	self.add_child(sfx_player)
+
+func play_bg_music(audioTrack):
+	self.stream = audioTrack
+	self.play()
+
+func stop_bg_music():
+	self.stop()
+
+func play_sfx(sfx):
+	print("Playing sound effect!")
+	sfx_player.stream = sfx
+	sfx_player.play()
--- scripts/combat_scene.gd
+++ scripts/combat_scene.gd
@@ -3,6 +3,7 @@ class_name CombatScene
 
 @export var enemyWaves: Array[EnemyWave]
 @export var doorOpensAfterTurns = 3
+@export var bgm: AudioStream
 
 func _init():
 	print("Combat Scene")
