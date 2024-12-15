95b0a24cc0bd74bbd823c16cba92099eb31e4e60
Pinguino Sod <pinguino.sod@gmail.com>
2024-12-15 13:59:49 +0100

    add hitpoints on ui, fix movementcost

--- label_movement_cost.gd
+++ label_movement_cost.gd
@@ -2,6 +2,6 @@ extends Label
 
 func _process(delta: float) -> void:
 	if (get_node("/root/main/Player").currentWeapon):
-		text = str(get_node("/root/main/Player").currentWeapon.movementCost)
+		text = str(get_node("/root/main/Player").currentWeapon.movementCost + 1)
 	else:
 		text = "1"
